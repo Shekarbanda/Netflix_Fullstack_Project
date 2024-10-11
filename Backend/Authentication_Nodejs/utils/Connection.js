455ef63c969c2d907615279721cef249f72a0780
BANDA SHEKAR <71442078+shekarlee@users.noreply.github.com>
2024-10-11 20:47:32 +0530

    Updated

--- Backend/Authentication_Nodejs/utils/Connection.js
+++ Backend/Authentication_Nodejs/utils/Connection.js
@@ -6,9 +6,12 @@ dotenv.config({
 })
 
 function dbConnection(){
-mongoose.connect(process.env.DB_URI).then(console.log("db connected")).catch((err)=>{
-    console.log(err);
-});
+    mongoose.connect(process.env.DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connected to MongoDB'))
+    .catch((err) => console.log('Error connecting to MongoDB:', err));
 }
+
+
+
 
 module.exports = dbConnection;
