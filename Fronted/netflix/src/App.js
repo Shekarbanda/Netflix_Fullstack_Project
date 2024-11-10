65975d93b1aa6c7c4b41c0ea01909e18b1701476
BANDA SHEKAR <71442078+Shekarbanda@users.noreply.github.com>
2024-11-10 22:45:58 +0530

    updated

--- Fronted/netflix/src/App.js
+++ Fronted/netflix/src/App.js
@@ -2,9 +2,31 @@ import './App.css';
 import Body from './Components/Body';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
 
 
 function App() {
+  const url = useSelector((state)=>state.backend.url);
+  const nav = useNavigate();
+  useEffect(()=>{
+    is_login();
+  },[]);
+
+  async function is_login() {
+    try {
+        const is_user = await axios.get(url, {
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            withCredentials: true
+        });
+    } catch (err) {
+        
+    }
+    
+}
   return (
     <>
       <Header></Header>
